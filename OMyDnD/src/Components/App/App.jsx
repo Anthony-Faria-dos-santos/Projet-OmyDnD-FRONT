ba216fc2a393fe926ff5acf786cf0fc78b8e61ea
Guillaume Bréchaire <g.brechaire@gmail.com>
2024-02-08 05:02:44 +0100

    feat(Auth_JWT): test form

--- OMyDnD/src/Components/App/App.jsx
+++ OMyDnD/src/Components/App/App.jsx
@@ -1,4 +1,4 @@
-import Contact from '../Contact'
+import SignUp from "../auth/SignUp/SignUp.jsx"
 
 
 function App() {
@@ -6,6 +6,7 @@ function App() {
   return (
     <div>
       <h1>App</h1>
+      <SignUp />
     </div>
   )
 }
