8a2aa92b0c41357bc46306eb91159c2db66b55fd
Guillaume Bréchaire <g.brechaire@gmail.com>
2024-02-08 05:00:42 +0100

    feat(Auth_JWT): add Provider from redux

--- OMyDnD/src/main.jsx
+++ OMyDnD/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import store from './store'
 import App from './Components/App/App.jsx'
 import '../src/styles/index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
