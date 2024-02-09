217bd53e0a527c0daeaab3c939e14649a529badf
Guillaume Bréchaire <g.brechaire@gmail.com>
2024-02-09 07:43:22 +0100

    feat(Auth_JWT): Set up Router for Header/SignIn/SignUp

--- OMyDnD/src/Components/App/App.jsx
+++ OMyDnD/src/Components/App/App.jsx
@@ -1,15 +1,20 @@
-import SignUp from "../auth/SignUp/SignUp.jsx"
-import Header from "../Header/index.jsx"
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignIn from "../auth/SignIn/SignIn.jsx";
+import SignUp from "../auth/SignUp/SignUp.jsx";
+import Header from "../Header/index.jsx";
 
 function App() {
-
   return (
-    <div>
-      <Header />
-      <SignUp />
-    </div>
-  )
+    <Router>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
