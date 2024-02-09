a7cf5588034a52c429619a816cea51a93707e5db
Guillaume Bréchaire <g.brechaire@gmail.com>
2024-02-09 07:35:39 +0100

    feat(Auth_JWT): Header SignIn SignUp functionnality

--- OMyDnD/src/Components/App/App.jsx
+++ OMyDnD/src/Components/App/App.jsx
@@ -1,10 +1,12 @@
 import SignUp from "../auth/SignUp/SignUp.jsx"
+import Header from "../Header/index.jsx"
 
 
 function App() {
 
   return (
     <div>
+      <Header />
       <SignUp />
     </div>
   )
