baeec5e3759598acd036644147bd374619c71b70
Anthony-Faria-dos-santos <anthony.faria-dos-santos@oclock.school>
2024-02-16 17:40:07 +0100

    feat: Add Race searchBar functionality

--- OMyDnD/src/Components/Search/index.jsx
+++ OMyDnD/src/Components/Search/index.jsx
@@ -1,50 +1,64 @@
 import { useState } from "react";
 import spellsData from "../../data/spells.json";
 import classesData from "../../data/classes.json";
+import raceData from "../../data/races.json";
 
+// Composant dédié à la recherche d'informations dans le sanctuaire au moyen de barres de recherche.
 function Search() {
+  // Gestions des states des différentes barres de recherche et de leur menu déroulant
+
+  // Recherche de sorts, permet de définir un état initial pour la barre de recherche et le menu déroulant
   const [querySpell, setQuerySpell] = useState("");
   const [selectedSpell, setSelectedSpell] = useState(null);
   const [showDropdownSpell, setShowDropdownSpell] = useState(false);
 
+  // Recherche de classes, permet de définir un état initial pour la barre de recherche et le menu déroulant
   const [queryClass, setQueryClass] = useState("");
   const [showDropdownClass, setShowDropdownClass] = useState(false);
 
+  // Recherche de races, permet de définir un état initial pour la barre de recherche et le menu déroulant
+  const [queryRace, setQueryRace] = useState("");
+  const [showDropdownRace, setShowDropdownRace] = useState(false);
+
+  // Fonctions pour gérer la sélection d'un sort dans le menu déroulant
   const handleSelectSpell = (spell) => {
+    // cible le sort sélectionné via la fonction setSelectedSpell
     setSelectedSpell(spell);
+    // Effacer l'affichage du menu déroulant
     setShowDropdownSpell(false);
+    // Effacer la recherche de sort lorsqu'un sort est sélectionné
     setQuerySpell("");
-    // Effacer la recherche de classe lorsqu'un sort est sélectionné
-    setQueryClass("");
   };
 
-  const handleSelectClass = (cls) => {
-    // Naviguer vers l'URL spécifique de la classe sélectionnée dans un nouvel onglet
-    const classUrl = `/sanctuary/classes/${cls.index}`;
-    window.open(classUrl, "_blank");
-    setShowDropdownClass(false);
-    setQueryClass("");
-    // Effacer la sélection de sort lorsqu'une classe est sélectionnée
-    setQuerySpell("");
+  // Fonction pour fermer la boîte de résultat
+  const handleCloseResult = () => {
     setSelectedSpell(null);
   };
 
+  // Fonctions pour gérer la sélection d'une classe dans le menu déroulant
+  const handleSelectClass = (cls) => {
+    // Pointe vers l'URL de la classe sélectionnée
+    const classUrl = `/sanctuary/classes/${cls.index}`;
+    // On s'assure d'ouvrir le lien dans un nouvel onglet
+    window.open(classUrl, "_blank");
+    setShowDropdownClass(false);
+    setQueryClass("");
+  };
+
+  // Fonctions pour gérer la sélection d'une race dans le menu déroulant
+  const handleSelectRace = (race) => {
+    const raceUrl = `/sanctuary/races/${race.index}`;
+    window.open(raceUrl, "_blank");
+    setShowDropdownRace(false);
+    setQueryRace("");
+  };
+
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        width: "100%",
-        marginTop: "50px",
-        position: "relative",
-        fontFamily: "sans-serif",
-      }}
-    >
+    <div style={containerStyle}>
       {/* Barre de recherche pour les sorts */}
       <input
         type="search"
+        name="querySpell"
         placeholder="Rechercher un sort..."
         value={querySpell}
         autoComplete="off"
@@ -52,6 +66,7 @@ function Search() {
         onFocus={() => setShowDropdownSpell(true)}
         style={inputStyle}
       />
+
       {/* Menu déroulant pour les sorts */}
       {showDropdownSpell && querySpell && (
         <ul style={dropdownStyle}>
@@ -71,6 +86,26 @@ function Search() {
             ))}
         </ul>
       )}
+
+      {/* Affichage du sort sélectionné  */}
+      {selectedSpell && (
+        <div style={resultStyle}>
+          <button onClick={handleCloseResult} style={closeButtonStyle}>
+            X
+          </button>
+          <h2 style={{ fontWeight: "bold", textAlign: "center" }}>
+            {selectedSpell.name}
+          </h2>
+          {/* Affichage des détails du sort */}
+          {Object.entries(selectedSpell).map(([key, value]) => (
+            <p key={key}>
+              <strong>{key}</strong>:{" "}
+              {typeof value === "object" ? JSON.stringify(value) : value}
+            </p>
+          ))}
+        </div>
+      )}
+
       {/* Barre de recherche pour les classes */}
       <input
         type="search"
@@ -81,6 +116,7 @@ function Search() {
         onFocus={() => setShowDropdownClass(true)}
         style={inputStyle}
       />
+
       {/* Menu déroulant pour les classes */}
       {showDropdownClass && queryClass && (
         <ul style={dropdownStyle}>
@@ -100,23 +136,64 @@ function Search() {
             ))}
         </ul>
       )}
-      {/* Affichage du sort sélectionné  */}
-      {selectedSpell && (
-        <div style={resultStyle}>
-          <h2 style={{ fontWeight: "bold", textAlign: "center" }}>
-            {selectedSpell.name}
-          </h2>
-          {Object.entries(selectedSpell).map(([key, value]) => (
-            <p key={key}>
-              <strong>{key}</strong>:{" "}
-              {typeof value === "object" ? JSON.stringify(value) : value}
-            </p>
-          ))}
-        </div>
+
+      {/* Barre de recherche pour les races */}
+      <input
+        type="search"
+        placeholder="Rechercher une race..."
+        value={queryRace}
+        autoComplete="off"
+        onChange={(e) => setQueryRace(e.target.value)}
+        onFocus={() => setShowDropdownRace(true)}
+        style={inputStyle}
+      />
+
+      {/* Menu déroulant pour les races */}
+      {showDropdownRace && queryRace && (
+        <ul style={dropdownStyle}>
+          {raceData
+            .filter((race) =>
+              race.name.toLowerCase().includes(queryRace.toLowerCase())
+            )
+            .slice(0, 20)
+            .map((race, index) => (
+              <li
+                key={index}
+                onClick={() => handleSelectRace(race)}
+                style={itemStyle}
+              >
+                {race.name}
+              </li>
+            ))}
+        </ul>
       )}
     </div>
   );
 }
+
+// Styles réutilisés pour les différents éléments de notre composant
+
+const containerStyle = {
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  width: "100%",
+  marginTop: "50px",
+  position: "relative",
+  fontFamily: "sans-serif",
+};
+
+const closeButtonStyle = {
+  position: "absolute",
+  top: "5px",
+  right: "5px",
+  border: "none",
+  background: "transparent",
+  cursor: "pointer",
+  fontWeight: "bold",
+  fontSize: "20px",
+};
 
 const inputStyle = {
   padding: "10px",
@@ -161,7 +238,7 @@ const resultStyle = {
   borderRadius: "8px",
   boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
   position: "absolute",
-  top: "100px",
+  top: "150px",
   fontFamily: "Cursive",
 };
 
