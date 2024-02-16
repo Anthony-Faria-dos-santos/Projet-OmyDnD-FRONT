75c00ed271f7aeaebab05255f62efa243cf44dad
Anthony-Faria-dos-santos <anthony.faria-dos-santos@oclock.school>
2024-02-16 18:04:29 +0100

    feat(Search): Link the /search page with the MagnifyingGlassCircleIcon inside the header

--- OMyDnD/src/Components/Header/index.jsx
+++ OMyDnD/src/Components/Header/index.jsx
@@ -100,7 +100,9 @@ function Header() {
           >
             <span className="sr-only">Ouvrir bar latérale</span>
             <UserCircleIcon
-              className={`text-gray-400 hover:text-gray-600 h-10 w-10 ${isLoggedIn ? "connexion-color" : "text-gray-400"}`}
+              className={`text-gray-400 hover:text-gray-600 h-10 w-10 ${
+                isLoggedIn ? "connexion-color" : "text-gray-400"
+              }`}
               aria-hidden="true"
             />
           </button>
@@ -201,7 +203,7 @@ function Header() {
         </Popover.Group>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
           <Link
-            to="#"
+            to="/search"
             className="text-sm font-semibold leading-6 text-gray-900"
           >
             <MagnifyingGlassCircleIcon
