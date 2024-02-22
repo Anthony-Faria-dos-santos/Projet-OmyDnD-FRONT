7845d26786acf26d0ad40f68a2592b9b4afd3966
Thomas-Trehou <thomastrehou.pro@gmail.com>
2024-02-22 13:20:27 +0100

    Fix(Spells):remove null option value from select type

--- OMyDnD/src/Components/Sanctuary/Attributes/AttributeSpell.jsx
+++ OMyDnD/src/Components/Sanctuary/Attributes/AttributeSpell.jsx
@@ -43,7 +43,6 @@ const SearchForm = () => {
                   className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-gray-500 sm:text-sm sm:leading-6 [&_*]:text-black" 
                   value={type} 
                   onChange={(e) => setType(e.target.value)}>
-                    <option value="">Intitulé</option>
                     <option value="name">Nom</option>
                     <option value="range">Distance</option>
                     <option value="casting_time">Temps d'incantation</option>
