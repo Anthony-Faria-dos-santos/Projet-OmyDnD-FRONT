dc7db015289c26e7699d78ad1405c8cf10695c9c
Anthony-Faria-dos-santos <anthony.faria-dos-santos@oclock.school>
2024-02-22 17:35:08 +0100

    Initial deploy Heroku

---
+++ OMyDnD/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { join } from 'path';
+const app = express();
+
+// Utilisez directement express.static et join importé
+app.use(express.static(join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(join(__dirname, 'dist', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
