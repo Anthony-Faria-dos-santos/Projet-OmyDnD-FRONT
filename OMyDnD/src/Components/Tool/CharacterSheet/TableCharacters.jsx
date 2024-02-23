0d3006e8ac39bfc22421e271c3cc3b76a6abd010
Guillaume Bréchaire <g.brechaire@gmail.com>
2024-02-23 04:57:27 +0100

    feat(CharacterCreator) : Add props and condition to update component tableCharacter

--- OMyDnD/src/Components/Tool/CharacterSheet/TableCharacters.jsx
+++ OMyDnD/src/Components/Tool/CharacterSheet/TableCharacters.jsx
@@ -1,7 +1,10 @@
+/* eslint-disable react/prop-types */
 import { useSelector } from 'react-redux';
 
-function TableCharacters() {
+function TableCharacters({ setName, setLevel, setHealth, setBonusHealth, setExperience, setInspiration }) {
+
     const character = useSelector((state) => state.character.selectedCharacter);
+
     console.log('Character:', character);
 
     const characterAttributes = [
@@ -13,7 +16,7 @@ function TableCharacters() {
         { label: "Level", id: "level", value: character?.level, type: "number", readOnly: false, colSpan: "sm:col-span-1" },
         { label: "Experience", id: "experience", value: character?.experience, type: "number", readOnly: false, colSpan: "sm:col-span-1" },
         { label: "Vie de départ", id: "starting_health", value: character?.classe_starting_health, type: "number", readOnly: true, colSpan: "sm:col-span-1" },
-        { label: "Dé de vie", id: "health_dice", value: character?.classe_health_dice, type: "number", readOnly: true, colSpan: "sm:col-span-1" },
+        { label: "Dé de vie", id: "health_dice", value: character?.classe_health_dice, type: "text", readOnly: true, colSpan: "sm:col-span-1" },
         { label: "Vie", id: "health", value: character?.health, type: "number", readOnly: false, colSpan: "sm:col-span-1" },
         { label: "Bonus de vie", id: "bonus_health", value: character?.bonus_health, type: "number", readOnly: false, colSpan: "sm:col-span-1" },
         { label: "Langues", id: "race_languages", value: character?.race_languages, type: "text", readOnly: true, colSpan: "sm:col-span-2 justify-start" },
@@ -31,7 +34,7 @@ function TableCharacters() {
                             <label htmlFor={attr.id} className="block text-sm font-medium leading-6 text-white">
                                 {attr.label}
                             </label>
-                            <div className="mt-2 rounded-md  ring-1 ring-inset ring-white/10 focus-within:ring-2 focus-within:ring-inset focus-within:ring-gray-500">
+                            <div className="mt-2 rounded-md ring-1 ring-inset ring-white/10 focus-within:ring-2 focus-within:ring-inset focus-within:ring-gray-500">
                                 <input
                                     type={attr.type}
                                     name={attr.id}
@@ -39,7 +42,22 @@ function TableCharacters() {
                                     autoComplete={attr.id}
                                     readOnly={attr.readOnly}
                                     className={`text-center block w-full rounded-md border-gray-300 py-1.5 text-gray-900 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${attr.readOnly === false ? 'bg-green-500' : 'text-white bg-white/5'}`}
-                                    defaultValue={attr.value}
+                                    defaultValue={attr.value || ''}
+                                    onChange={(e) => {
+                                        if (attr.id === "name") {
+                                            setName(e.target.value);
+                                        } else if (attr.id === "level") {
+                                            setLevel(e.target.value);
+                                        } else if (attr.id === "health") {
+                                            setHealth(e.target.value);
+                                        } else if (attr.id === "bonus_health") {
+                                            setBonusHealth(e.target.value);
+                                        } else if (attr.id === "experience") {
+                                            setExperience(e.target.value);
+                                        } else if (attr.id === "inspiration") {
+                                            setInspiration(e.target.value);
+                                        }
+                                    }}
                                 />
                             </div>
                         </div>
