2c197a77527040f24fb42760fc14261fa6da63ed
Anthony-Faria-dos-santos <anthony.faria-dos-santos@oclock.school>
2024-02-23 19:35:20 +0100

    fix(contact): Integration of validation of contact form fields

--- OMyDnD/src/Components/Contact/Form/index.jsx
+++ OMyDnD/src/Components/Contact/Form/index.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useForm, ValidationError } from '@formspree/react';
+import { useForm } from '@formspree/react';
 import { Switch } from "@headlessui/react";
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -9,6 +9,64 @@ function classNames(...classes) {
 function Form() {
   const [agreed, setAgreed] = useState(false);
   const [state, handleSubmit] = useForm("mgegkjbw");
+  const [errors, setErrors] = useState({});
+  const [formState, setFormState] = useState({
+    email: '',
+    message: '',
+    phone: '',
+    firstname: '',
+    lastname: '',
+  });
+
+  const validateForm = () => {
+    const newErrors = {};
+    const phoneRegex = /^((?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-8][02-9])\s*(?:[.-]\s*)?([0-9]{4})|(?:\+33\s?)(?:\(0\))?[1-9](?:[\s.-]?[0-9]{2}){4})$/;
+
+    if (!formState.email.includes('@')) {
+      newErrors.email = "Vous devez renseigner un email valide";
+    }
+
+    if (formState.message === '' || formState.message.length < 10) {
+      newErrors.message = "Vous devez renseigner un message de 10 caractères minimum";
+    }
+
+    if (formState.phone === '' || formState.phone.length < 8 || !phoneRegex.test(formState.phone)) {
+      newErrors.phone = "Vous devez renseigner un numéro de téléphone valide";
+    }
+
+    if (formState.firstname === '' || formState.firstname.length < 2) {
+      newErrors.name = "Vous devez renseigner un prénom de 2 caractères minimum";
+    }
+
+    if (formState.lastname === '' || formState.lastname.length < 2) {
+      newErrors.lastName = "Vous devez renseigner un nom de famille de 2 caractères minimum";
+    }
+    return newErrors;
+  };
+
+  const handleSubmitVerify = (e) => {
+    e.preventDefault();
+    const formErrors = validateForm();
+    if (Object.keys(formErrors).length > 0) {
+      setErrors(formErrors);
+      return;
+    } else {
+      handleSubmit(e);
+    }
+  };
+
+  const handleChange = (e) => {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+    if (errors[e.target.name]) {
+      const newErrors = { ...errors };
+      delete newErrors[e.target.name];
+      setErrors(newErrors)
+    }
+  };
+
+
+
+
   if (state.succeeded) {
     return <p>Merci, nous vous répondrons dans les meilleurs délais !</p>;
   }
@@ -28,7 +86,7 @@ function Form() {
         action="https://formspree.io/f/mgegkjbw"
         method="POST"
         className="mx-auto mt-16 max-w-xl sm:mt-20"
-        onSubmit={handleSubmit}
+        onSubmit={handleSubmitVerify}
       >
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
           <div>
@@ -41,11 +99,14 @@ function Form() {
             <div className="mt-2.5">
               <input
                 type="text"
-                name="first-name"
+                name="firstname"
                 id="first-name"
                 autoComplete="given-name"
+                value={formState.firstname}
+                onChange={handleChange}
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
+              {errors.firstname && <p style={{ color: "red" }}>{errors.firstname}</p>}
             </div>
           </div>
           <div>
@@ -58,11 +119,14 @@ function Form() {
             <div className="mt-2.5">
               <input
                 type="text"
-                name="last-name"
+                name="lastname"
                 id="last-name"
                 autoComplete="family-name"
+                value={formState.lastname}
+                onChange={handleChange}
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
+              {errors.lastname && <p style={{ color: "red" }}>{errors.lastname}</p>}
             </div>
           </div>
 
@@ -78,15 +142,13 @@ function Form() {
               <input
                 type="email"
                 name="email"
+                value={formState.email}
                 id="email"
                 autoComplete="email"
+                onChange={handleChange}
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
-              <ValidationError
-                prefix="Email"
-                field="email"
-                errors={state.errors}
-              />
+              {errors.email && <p style={{ color: "red" }}>{errors.email}</p>}
             </div>
           </div>
           <div className="sm:col-span-2">
@@ -104,7 +166,7 @@ function Form() {
                 <select
                   id="country"
                   name="country"
-                  className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-black focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                  className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-3 text-black focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
                 >
                   <option>US</option>
                   <option>CA</option>
@@ -114,14 +176,18 @@ function Form() {
               </div>
               <input
                 type="tel"
-                name="phone-number"
+                name="phone"
                 id="phone-number"
                 autoComplete="tel"
+                value={formState.phone}
+                onChange={handleChange}
                 className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
+              {errors.phone && <p style={{ color: "red" }}>{errors.phone}</p>}
             </div>
           </div>
           <div className="sm:col-span-2">
+            {/* Champ message */}
             <label
               htmlFor="message"
               className="block text-sm font-semibold leading-6 text-white"
@@ -134,13 +200,10 @@ function Form() {
                 id="message"
                 rows={4}
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                defaultValue={""}
+                value={formState.message}
+                onChange={handleChange}
               />
-              <ValidationError
-                prefix="message"
-                field="message"
-                errors={state.errors}
-              />
+              {errors.message && <p style={{ color: "red" }}>{errors.message}</p>}
             </div>
           </div>
           <Switch.Group as="div" className="flex gap-x-4 sm:col-span-2">
@@ -173,14 +236,19 @@ function Form() {
           </Switch.Group>
         </div>
         <div className="mt-10">
+          {/* Bouton d'envoi */}
           <button
             type="submit"
-            disabled={state.submitting}
-            className="flex w-full justify-center rounded-md bg-gray-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
+            disabled={state.submitting || !agreed}
+            className={
+              agreed ? "flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600" : "flex w-full justify-center rounded-md bg-gray-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
+            }
           >
-            Envoyez
+            Envoyer
           </button>
         </div>
+        {/* Message de succès */}
+        {state.succeeded && <p>Merci, nous vous répondrons dans les meilleurs délais !</p>}
       </form>
     </div>
   );
