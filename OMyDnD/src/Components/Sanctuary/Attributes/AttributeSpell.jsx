20662b5aca0b5e264846094c2c2769c284b226a9
Anthony-Faria-dos-santos <anthony.faria-dos-santos@oclock.school>
2024-02-23 12:05:15 +0100

    fix(search): fix problem when no data set

--- OMyDnD/src/Components/Sanctuary/Attributes/AttributeSpell.jsx
+++ OMyDnD/src/Components/Sanctuary/Attributes/AttributeSpell.jsx
@@ -10,12 +10,16 @@ const SearchForm = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
+
     try {
       const response = await fetch(
         `${API_URL}/api/options/spells/?type=${type}&value=${value}`
       );
+
       const data = await response.json();
+      if (data.error) {
+        return setData([]);
+      }
       setData(data);
     } catch (error) {
       console.error(error);
@@ -24,52 +28,56 @@ const SearchForm = () => {
   };
 
   return (
-      <div className="bg-gray-900 m-2">
-        <main className="pb-10 mx-auto max-w-7xl sm:px-6 sm:pt-16 lg:px-8 flex flex-col place-items-center">
-          <img src="../images/spells/spells.jpg" className="h-full w-8/12 object-cover object-center sm:rounded-lg mx-auto" />
+    <div className="bg-gray-900 m-2">
+      <main className="pb-10 mx-auto max-w-7xl sm:px-6 sm:pt-16 lg:px-8 flex flex-col place-items-center">
+        <img src="../images/spells/spells.jpg" className="h-full w-8/12 object-cover object-center sm:rounded-lg mx-auto" />
 
-          <div className="p-2 bg-gray-800 rounded-lg mt-8 w-2/6">
+        <div className="p-2 bg-gray-800 rounded-lg mt-8 w-2/6">
 
-            <form onSubmit={handleSubmit} 
-                className="space-y-6"
-                action="#"
-                method="GET">
-              <label htmlFor="type"
-                className="block text-sm font-medium leading-6 text-gray-50">
-                  Que voulez-vous rechercher ?
-              </label>
-                <select id="type" 
-                  name="type"
-                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-gray-500 sm:text-sm sm:leading-6 [&_*]:text-black" 
-                  value={type} 
-                  onChange={(e) => setType(e.target.value)}>
-                    <option value="name">Nom</option>
-                    <option value="range">Distance</option>
-                    <option value="casting_time">Temps d'incantation</option>
-                    <option value="level">Niveau</option>
-                    <option value="school">Ecole de magie</option>
-                    <option value="class">Classe</option>
-                    <option value="archetype">Archétype</option>
-                    <option value="domains">Domaine</option>
-                    <option value="oaths">Serment</option>
-                    <option value="circles">Cercle</option>
-                    <option value="patrons">Patron</option>
-                </select>
-                <input type="text" 
-                  id="value" 
-                  name="value" 
-                  className="block w-full rounded-md border-0 py-1.5 px-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
-                  placeholder="Votre recherche"
-                  value={value} onChange={(e) => setValue(e.target.value)} />
-              <button type="submit"
-                className="flex w-full justify-center rounded-md bg-gray-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600">
-                  Rechercher
-              </button>
-            </form>
+          <form onSubmit={handleSubmit}
+            className="space-y-6"
+            action="#"
+            method="GET">
+            <label htmlFor="type"
+              className="block text-sm font-medium leading-6 text-gray-50">
+              Que voulez-vous rechercher ?
+            </label>
+            <select
+              id="type"
+              name="type"
+              className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-gray-500 sm:text-sm sm:leading-6 [&_*]:text-black"
+              value={type}
+              onChange={(e) => setType(e.target.value)}>
 
-          </div>
+              <option value="" disabled>Choisissez un type</option>
+              <option value="name">Nom</option>
+              <option value="range">Distance</option>
+              <option value="casting_time">Temps d&apos;incantation</option>
+              <option value="level">Niveau</option>
+              <option value="school">Ecole de magie</option>
+              <option value="class">Classe</option>
+              <option value="archetype">Archétype</option>
+              <option value="domains">Domaine</option>
+              <option value="oaths">Serment</option>
+              <option value="circles">Cercle</option>
+              <option value="patrons">Patron</option>
+            </select>
+            <input
+              type="text"
+              id="value"
+              name="value"
+              className="block w-full rounded-md border-0 py-1.5 px-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
+              placeholder="Votre recherche"
+              value={value} onChange={(e) => setValue(e.target.value)} />
+            <button type="submit"
+              className="flex w-full justify-center rounded-md bg-gray-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600">
+              Rechercher
+            </button>
+          </form>
 
-          <div className="lg:grid lg:grid-cols-3 place-content-around mt-10">
+        </div>
+
+        <div className="lg:grid lg:grid-cols-3 place-content-around mt-10">
           {data.map((item, index) => (
             <div className="bg-gray-800 rounded-lg p-2 m-2" key={index}>
               <div className="text-gray-50 bg-gray-900 rounded-lg p-2 font-semibold uppercase text-center">
@@ -153,8 +161,9 @@ const SearchForm = () => {
           ))}
         </div>
 
-        </main>
-      </div>
-  )};
+      </main>
+    </div>
+  )
+};
 
 export default SearchForm;
